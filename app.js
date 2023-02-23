c66077dfc7e5535a49a311874e196c1443bef074
Folawewo Anthony <famwallexie@gmail.com>
2023-02-23 21:53:12 +0100

    authenticating user

--- app.js
+++ app.js
@@ -1,8 +1,22 @@
-const express = require("express");
-require("./services/mongo");
+const express = require('express');
+const cookieParser = require('cookie-parser');
+const mongoose = require('mongoose');
+const authMiddleware = require('./middleware/auth');
+const authController = require('./controllers/auth');
+const userRoutes = require('./routes/user');
+
+require('./services/mongo');
 
 const app = express();
 
-app.use(express.json())
+app.use(express.json());
+app.use(cookieParser());
+app.use(authMiddleware);
+
+app.post('/signup', authController.signup);
+app.post('/login', authController.login);
+app.post('/logout', authController.logout);
+
+app.use('/user', userRoutes);
 
 module.exports = app;
--- routes/user.js
+++ routes/user.js
@@ -3,4 +3,35 @@ const User = require('../models/user');
 
 const router = express.Router();
 
-router.get('/profile')
+router.get('/profile', async (req, res) => {
+  try {
+    const user = await User.findById(req.userId);
+    if (!user) {
+      return res.status(404).json({ message: 'User not found' });
+    }
+    res.json(user);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: 'Internal server error' });
+  }
+});
+
+router.put('/profile', async (req, res) => {
+  try {
+    const { username, email } = req.body;
+    const user = await User.findByIdAndUpdate(
+      req.userId,
+      { username, email },
+      { new: true }
+    );
+    if (!user) {
+      return res.status(404).json({ message: 'User not found' });
+    }
+    res.json(user);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: 'Internal server error' });
+  }
+});
+
+module.exports = router;
